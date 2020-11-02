0011f4f01551b6a8ec132445d00f772a3bd15147
unknown <9ealves@gmail.com>
2020-11-02 02:54:49 -0300

    Criando o componente disappeared-detail

---
+++ src/app/modules/disappeared/disappeared-detail/disappeared-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-disappeared-detail',
+  templateUrl: './disappeared-detail.component.html',
+  styleUrls: ['./disappeared-detail.component.css']
+})
+export class DisappearedDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
