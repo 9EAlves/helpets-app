b4bbf8a0187f7a92ab0b5038b07a041e22f97064
unknown <9ealves@gmail.com>
2020-11-02 02:24:39 -0300

    Disappeared-card - Criação e implementação

--- src/app/modules/disappeared/disappeared.component.ts
+++ src/app/modules/disappeared/disappeared.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription} from 'rxjs'
-import { Disappeared} from './../../core/models/disappeared.model'
-import { DisappearedService} from './../../core/services/disappeared.service'
+import { Subscription } from 'rxjs'
+import { Disappeared } from './../../core/models/disappeared.model'
+import { DisappearedService } from './../../core/services/disappeared.service'
 
 
 @Component({
@@ -9,12 +9,12 @@ import { DisappearedService} from './../../core/services/disappeared.service'
   templateUrl: './disappeared.component.html',
   styleUrls: ['./disappeared.component.css']
 })
-export class DisappearedComponent implements OnInit, OnDestroy{
+export class DisappearedComponent implements OnInit, OnDestroy {
 
   private HttpRequest: Subscription
   Disappeared: Disappeared[]
 
-  constructor( private disappearedService: DisappearedService) { }
+  constructor(private disappearedService: DisappearedService) { }
 
   ngOnInit(): void {
     this.findAllDisappeared()
@@ -24,12 +24,12 @@ export class DisappearedComponent implements OnInit, OnDestroy{
     this.HttpRequest.unsubscribe()
   }
 
-  findAllDisappeared(): void{
+  findAllDisappeared(): void {
     this.HttpRequest = this.disappearedService.findAllDisappeared().subscribe(response => {
       /** Sucesso da requisição */
       this.Disappeared = response.body['data']
-     // console.log(response)
-    }, err =>{
+      // console.log(response)
+    }, err => {
       /** Erro da requisição */
       console.log(err)
     })
--- src/app/modules/disappeared/disappeared.module.ts
+++ src/app/modules/disappeared/disappeared.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatInputModule } from '@angular/material/input';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatSelectModule} from '@angular/material/select';
-import {MatCardModule} from '@angular/material/card';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field'
 
 import { DisappearedCardComponent } from './disappeared-card/disappeared-card.component';
 import { DisappearedDetailComponent } from './disappeared-detail/disappeared-detail.component';
-import { ComponentsModule} from './../../components/components.module';
+import { ComponentsModule } from './../../components/components.module';
 import { NewDisappearedComponent } from './new-disappeared/new-disappeared.component';
 import { DisappearedRoutingModule } from './disappeared-routing.module';
 import { DisappearedComponent } from './disappeared.component';
@@ -42,6 +42,6 @@ import { DisappearedComponent } from './disappeared.component';
     MatIconModule,
     MatFormFieldModule
   ]
- 
+
 })
 export class DisappearedModule { }
