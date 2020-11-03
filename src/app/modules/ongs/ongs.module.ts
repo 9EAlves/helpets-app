39304af75d1534ac5b75b5027f131a94ca656c91
nat-crm <nataliatmrs@gmail.com>
2020-11-03 15:54:50 -0300

    Criando modulo ongs

---
+++ src/app/modules/ongs/ongs.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OngsRoutingModule } from './ongs-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OngsRoutingModule
+  ]
+})
+export class OngsModule { }
