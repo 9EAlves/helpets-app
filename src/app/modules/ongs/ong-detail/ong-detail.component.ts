564147987329628b300d9b36600423a4154e6c04
nat-crm <nataliatmrs@gmail.com>
2020-11-16 20:13:45 -0300

    Implementando navegacao entre ong e ong detail

--- src/app/modules/ongs/ong-detail/ong-detail.component.ts
+++ src/app/modules/ongs/ong-detail/ong-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ong-detail',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OngDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const ongId = this.activatedRoute.snapshot.params['userId']
+
   }
 
 }
