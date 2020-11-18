a30bf92a5d2b7067c37d7cb1bf120de6d8542a67
nat-crm <nataliatmrs@gmail.com>
2020-11-18 11:15:59 -0300

    IMplementando ong detail

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -3,6 +3,7 @@ import { User } from './../models/user.model';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { identifierName } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,11 @@ export class UserService {
 
   findAllUser(): Observable<HttpResponse<User[]>>{
     return this.http.get<User[]>(`${API_URL}/user/viewAll`, {observe: 'response'})
+  }
+
+  findOngById(ongId: String): Observable<HttpResponse<User>>{
+    return this.http.get<User>(`${API_URL}/user/viewOne/${ongId}`, {observe: 'response'})
+    
   }
 
 }
