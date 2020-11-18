6027cb97bf05bb76673fe2a85ac91ede24057d6c
nat-crm <nataliatmrs@gmail.com>
2020-11-18 14:31:12 -0300

    Implementando o loading component na ong detail

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
-import { MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { LoadingComponent } from './loading/loading.component';
 
