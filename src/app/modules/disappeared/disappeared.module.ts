7df9d5242a1070f28a95ae12eb30e2efcce106a9
unknown <9ealves@gmail.com>
2020-11-20 03:22:22 -0300

    New-disapperaed criando e implementando

--- src/app/modules/disappeared/disappeared.module.ts
+++ src/app/modules/disappeared/disappeared.module.ts
@@ -3,27 +3,28 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule} from '@angular/forms';
 
 
-import { DisappearedRoutingModule } from './disappeared-routing.module';
-import { DisappearedComponent } from './disappeared.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatInputModule } from '@angular/material/input';
-
-
 import {MatDividerModule} from '@angular/material/divider';
 import {MatSelectModule} from '@angular/material/select';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field'
+
 import { DisappearedCardComponent } from './disappeared-card/disappeared-card.component';
 import { DisappearedDetailComponent } from './disappeared-detail/disappeared-detail.component';
 import { ComponentsModule} from './../../components/components.module';
-
+import { NewDisappearedComponent } from './new-disappeared/new-disappeared.component';
+import { DisappearedRoutingModule } from './disappeared-routing.module';
+import { DisappearedComponent } from './disappeared.component';
 
 
 
 @NgModule({
-  declarations: [DisappearedComponent, DisappearedCardComponent, DisappearedDetailComponent],
+  declarations: [DisappearedComponent, DisappearedCardComponent, DisappearedDetailComponent, NewDisappearedComponent],
   imports: [
     CommonModule,
     DisappearedRoutingModule,
@@ -37,7 +38,9 @@ import { ComponentsModule} from './../../components/components.module';
     MatButtonModule,
     MatDialogModule,
     MatStepperModule,
-    MatInputModule
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule
   ]
  
 })
