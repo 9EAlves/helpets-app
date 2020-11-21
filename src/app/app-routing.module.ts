2e779b281898dac805433d26288144dc61306dcd
MarceloTonhetti <marcelotjunior2507@gmail.com>
2020-11-21 17:12:41 -0300

    Separando rota privada de admin (ainda falta fazer a autenticação)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -39,6 +39,10 @@ const routes: Routes = [
   {
     path: 'about',
     loadChildren: () => import('./modules/about-us/about-us.module').then(module => module.AboutUsModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin-panel/admin-panel.module').then(module => module.AdminPanelModule)
   }
 
 ];
