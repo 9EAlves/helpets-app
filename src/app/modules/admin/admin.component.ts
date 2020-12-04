6358953936970357541f8130169d4e5c898b5216
unknown <9ealves@gmail.com>
2020-12-04 04:25:58 -0300

    Recriando ADM panel e adicionando rate post para desaparecidos e adções

---
+++ src/app/modules/admin/admin.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Disappeared } from './../../core/models/disappeared.model';
+import { DisappearedService } from './../../core/services/disappeared.service';
+
+
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+  
+  ngOnInit(){
+    
+  }
+
+}
